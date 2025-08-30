dd2c47f757b92480e17391cca69592cd8ee5dc6f
AleksChek26 <check1984@gmail.com>
2025-08-30 12:22:02 +0500

    homework_14_2

--- src/category.py
+++ src/category.py
@@ -1,12 +1,12 @@
 from src.product import Product
+from src.exceptions import ZeroQuantityProduct
 
 
 class Category:
     """Класс для категорий товаров"""
-
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
     products_list: list = []
@@ -15,8 +15,60 @@ class Category:
         """Конструктор для категории товаров"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
         Category.products_list.extend(products)
+
+    def __str__(self) -> str:
+        """Метод, который возвращает строку в следующем виде:
+        *Название категории, количество продуктов: 200 шт.*"""
+        quantity_sum = 0
+        for product in self.__products:
+            quantity_sum += product.quantity
+        return f"{self.name}, количество продуктов: {quantity_sum} шт."
+
+    def add_product(self, product: Product) -> None:
+        """Метод для добавления продукта в атрибут products"""
+        if isinstance(product, Product):
+            try:
+                if product.quantity == 0:
+                    raise ZeroQuantityProduct("Попытка добавить товар с нулевым количеством")
+            except ZeroQuantityProduct as e:
+                print(str(e))
+            else:
+                self.__products.append(product)
+                Category.product_count += 1
+                Category.products_list.append(product)
+                print("Товар добавлен успешно")
+            finally:
+                print("Обработка добавления товара завершена")
+        else:
+            raise TypeError
+
+    @property
+    def products(self) -> str:
+        """Геттер возвращает строку со всеми продуктами в приватном атрибуте products"""
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{str(product)}\n"
+        return product_str
+
+    @property
+    def products_in_list(self) -> list:
+        """Геттер, который возвращает приватный атрибут products в виде списка"""
+        return self.__products
+
+    def middle_price(self) -> float:
+        """Метод, который подсчитывает средний ценник всех товаров в данной категории"""
+        quantity_sum = 0
+        price_sum = 0
+        for product in self.__products:
+            price_sum += product.price * product.quantity
+            quantity_sum += product.quantity
+        try:
+            return round(price_sum / quantity_sum, 2)
+        except ZeroDivisionError:
+            return 0
+
--- tests/test_product.py
+++ tests/test_product.py
@@ -9,3 +9,19 @@ def test_product_init(product: Product) -> None:
     assert product.description == "256GB, Серый цвет, 200MP камера"
     assert product.price == 180000.0
     assert product.quantity == 5
+
+def test_product_str(product: Product) -> None:
+    """Тестируем строковое отображение продукта"""
+    assert str(product) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+
+
+def test_product_init_invalid() -> None:
+    """Тестируем поведение конструктора при попытке добавить товар разного класса с нулевым количеством, то есть
+    вызываем исключение ValueError"""
+    with pytest.raises(ValueError) as e1:
+        Product("Бракованный товар", "Неверное количество", 1000.0, 0)
+        assert str(e1.value) == "Товар с нулевым количеством не может быть добавлен"
+
+def test_product_add(product: Product, other_product: Product) -> None:
+    """Тестируем метод получения полной стоимости всех выбранных товаров на складе"""
+    assert product + other_product == 2580000
